12729dd8776a433da52f1eb97ebcd6ebfee9ea26
Byron <elemantalcode@users.noreply.github.com>
2018-01-30 17:46:50 +0000

    Neated it up
    Removed pointless 'cd' and adhered to cleaner BASH code

--- recover-artifactory-rpm.bash
+++ recover-artifactory-rpm.bash
@@ -3,8 +3,6 @@
 # This works by trawling the .rpmcache/ tree to find an exact RPM name
 #  finds the SHA and trawls the filesystem to recover the exact RPM
 #  with the correct name.
-#
-# Needs lots of work - written in 7 minutes
 
 # Set the artifactory recover mount
 basename="/mnt/disk1/artifactory/data"
@@ -12,33 +10,30 @@ basename="/mnt/disk1/artifactory/data"
 # Set the destination recover directory
 recoverdir="/storage/recover"
 
-mkdir -p $recoverdir
+mkdir -p "$recoverdir"
 
-if [ -z "$1" ]; then
+if [[ -z "$1" ]]; then
     echo "Supply an RPM name"
-    exit 99
+    exit 1
 fi
 
 filename="$1"
 
-cd $basename
+if [[ -s "$recoverdir/$filename" ]]; then
+    echo "$filename is already recovered!"
+    exit 1
+fi
 
-# Find rpmcache name and directory attached
-fullrpmname=`find ./ -name "$1" | cut -d'.' -f2-`
-rpmdirname=`dirname "$basename/$fullrpmname" | tail -1`
+# Find rpmcache name and directory attached, but only 'local'
+fullrpmname=$( find $basename -name "$1" | grep local )
+rpmdirname=$( dirname "$fullrpmname" )
 
 # Grab the SHA to find the actual file
-cd "$rpmdirname"
-sha=`cat "$filename" | jq -r '.sha1Digest'`
-
-cd $basename
-
-fullshaname=`find ./ -name "$sha" | cut -d'.' -f2-`
-shadirname=`dirname "$basename/$fullshaname" | tail -1`
-
-cd "$shadirname"
+sha=$( jq -r '.sha1Digest' "$rpmdirname/$filename" )
+fullshaname=$( find "$basename" -name "$sha" )
+shadirname=$( dirname "$fullshaname" )
 
 # Copy the SHA named file to the actual RPM name and
 # change perms to be able to import correctly as arti user
-cp $sha $recoverdir/$filename
-chown artifactory: $recoverdir/*
+cp "$shadirname/$sha" "$recoverdir/$filename"
+chown artifactory: "$recoverdir"/*
